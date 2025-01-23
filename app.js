16ef9de671cb4b864a4f5cc82d53fb968d540f78
Berihuete Nunez, Engel <mastering23@gmail.com>
2025-01-23 14:50:08 -0500

    adding objs groceryList

---
+++ app.js
@@ -0,0 +1,13 @@
+// console.log("TESTING");
+
+// 1)Create a project called grocery-list ✅
+// 2)Create an array called groceryList✅
+const groceryList = [
+  { id: 1, name: "apple", price: 1.75, category: "fruit", quantity: 100 },
+  { id: 2, name: "banana", price: 0.25, category: "fruit", quantity: 137 },
+  { id: 3, name: "orange", price: 1.0, category: "fruit", quantity: 10 },
+  { id: 4, name: "broccoli", price: 3.0, category: "vegetable", quantity: 67 },
+  { id: 6, name: "milk", price: 5.75, category: "dairy", quantity: 90 },
+  { id: 7, name: "cheddar", price: 4.0, category: "dairy", quantity: 63 },
+  { id: 8, name: "sourdough", price: 5.5, category: "grains", quantity: 81 },
+];
